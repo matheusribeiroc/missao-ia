fee70eed182b295ba84b5ca1b69379b7ba5a13ea
M.ribeiro <140640049+matheusribeiroc@users.noreply.github.com>
2025-10-22 14:24:46 -0300

    234

--- java/script.js
+++ java/script.js
@@ -12,3 +12,33 @@ let perguntaAtual;
 let historiaFinal = "";
 
 botaoIniciar.addEventListener('clik,iniciaJogo');
+function iniciaJogo (){
+    atual = 0;
+    historiaFinal = "";
+    telaInicial.style.display = 'none';
+    caixaPerguntas.classList.remove("mostrar");
+    caixaAlternativas.classList.remove("mostrar");
+    caixaResultados.classList.remove("mostrar");
+    mostraPergunta();
+}
+
+function mostraPergunta (){
+    if (atual <= perguntas.length){
+        mostraResultados();
+        return;
+    }
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for (const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+    
+}
