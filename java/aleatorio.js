2dbe5ff70e80a509c2dc4bf37e4bd50f0e2bae2d
M.ribeiro <140640049+matheusribeiroc@users.noreply.github.com>
2025-11-03 09:48:02 -0300

    12345

---
+++ java/aleatorio.js
@@ -0,0 +1,6 @@
+const nomes = ["Matheus","Thomaz","Rodrigo","Lucas","Renato"];
+export function aleatorio (lista){
+    const posicao = Math.floor(Math.random()*lista.lenght);
+    return lista[posicao];
+}
+export const nome = aleatorio(nomes)
