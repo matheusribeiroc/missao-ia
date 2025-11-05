45a0a8883cd8fc2a886bb78c3e869cc73d94c630
M.ribeiro <140640049+matheusribeiroc@users.noreply.github.com>
2025-11-05 14:28:16 -0300

    345

--- java/perguntas.js
+++ java/perguntas.js
@@ -1,8 +1,16 @@
-export const perguntas = {
+export const perguntas = [
+    {
     enunciado:"Como um país pode gerenciar a transição para a economia do Trabalho 5.0 de forma a maximizar os benefícios da produtividade gerada pela IA, ao mesmo tempo que mitiga a crescente desigualdade social causada pela obsolescência de habilidades de milhões de trabalhadores? O que você faria?",
     alternativas:[
 }
 texto: "Como um país pode gerenciar a transição para a economia do Trabalho 5.0 de forma a maximizar os benefícios da produtividade gerada pela IA, ao mesmo tempo que mitiga a crescente desigualdade social causada pela obsolescência de habilidades de milhões de trabalhadores?";
 afirmacao:[
-    "O problema apresentado é o Dilema da Transição para o Trabalho 5.0, focado na polarização do mercado de trabalho devido à IA e à automação.A questão central é: Como gerenciar esta transição para maximizar a produtividade da IA, enquanto se mitiga a desigualdade social causada pela obsolescência de habilidades?Resumo das Alternativas:AlternativaCenárioJustificativaA: Favorável (Prosperidade Inclusiva)Ocorre com o investimento contínuo e massivo em requalificação (reskilling), financiado pela produtividade da própria automação.Libera o potencial humano (criatividade, ética) ao reduzir a carga horária de trabalho (ex: Semana de 4 Dias) e transforma os trabalhadores em Supervisores de IA, criando uma sociedade mais justa e inovadora.B: Desfavorável (Polarização e Estagnação)Ocorre quando o foco é apenas no lucro da eficiência e o governo falha em dar suporte e requalificação à classe média.Causa o desemprego estrutural e força a maioria para a "Gig Economy" de baixo valor, levando a uma grande desigualdade e instabilidade social.A afirmação justifica a Alternativa A como o único caminho economicamente sustentável e socialmente justo, pois garante que os benefícios da tecnologia sejam redistribuídos em forma de tempo, aprendizado e dignidade do trabalho."
+    "O problema apresentado é o Dilema da Transição para o Trabalho 5.0, focado na polarização do mercado de trabalho devido à IA e à automação.A questão central é: Como gerenciar esta transição para maximizar a produtividade da IA, enquanto se mitiga a desigualdade social causada pela obsolescência de habilidades?Resumo das Alternativas:AlternativaCenárioJustificativaA: Favorável (Prosperidade Inclusiva)Ocorre com o investimento contínuo e massivo em requalificação (reskilling), financiado pela produtividade da própria automação.Libera o potencial humano (criatividade, ética) ao reduzir a carga horária de trabalho (ex: Semana de 4 Dias) e transforma os trabalhadores em Supervisores de IA, criando uma sociedade mais justa e inovadora.B: Desfavorável (Polarização e Estagnação)Ocorre quando o foco é apenas no lucro da eficiência e o governo falha em dar suporte e requalificação à classe média.Causa o desemprego estrutural e força a maioria para a "Gig Economy" de baixo valor, levando a uma grande desigualdade e instabilidade social.A afirmação justifica a Alternativa A como o único caminho economicamente sustentável e socialmente justo, pois garante que os benefícios da tecnologia sejam redistribuídos em forma de tempo, aprendizado e dignidade do trabalho.";
+],
+proxima:1,
+},
+{ 
+  texto:"segunda alternativa";  
+}
 ]
+}
